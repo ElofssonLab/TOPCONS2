a6b0e0faeebbc7d12cc53cb5ea938a904d2dc63e
Nanjiang Shu <nanjiang.shu@bils.se>
2015-09-03 09:49:35 +0200

    clean up

--- topcons2_webserver/workflow/bak/copy_final_topo-2015-02-05-19.30.48.py
+++
@@ -1,6 +0,0 @@
-import os
-
-inPath = "../out/"
-for entry in os.listdir(inPath):
-    prot_name = entry.split("_")[0]
-    os.system("cp " + inPath + entry + "/Topcons/topcons.top " + "../out_topo/" + prot_name + ".top")
